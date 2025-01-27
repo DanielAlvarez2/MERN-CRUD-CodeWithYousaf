becf03cf6b652ab71432ad68708b2d308ab15193
Daniel Alvarez <daniel.yllanes@hotmail.com>
2025-01-27 01:58:29 -0500

    front-end gui completed

---
+++ client/src/Users.jsx
@@ -0,0 +1,43 @@
+import React,{useState} from 'react'
+import {Link} from 'react-router-dom'
+
+export default function Users(){
+    const [users, setUsers] = useState([{
+        Name:'Daniel',
+        Email:'daniel.alvarez@togglesoftware.com',
+        Age:50
+    }])
+    return(
+        <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
+            <div className='w-50 bg-white rounded p-3'>
+                <Link to='/create' className='btn btn-success'>Add +</Link>
+                <table className='table'>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Age</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map((user) => {
+                                return <tr>
+                                    <td>{user.Name}</td>
+                                    <td>{user.Email}</td>
+                                    <td>{user.Age}</td>
+                                    <td>
+                                    <Link to='/update' className='btn btn-success'>Update</Link>
+                                    <button>Delete</button>
+                                    </td>
+                                </tr>
+                            })
+                        }
+                    </tbody>
+                </table>
+
+            </div>
+        </div>
+    )
+}
