150be3a0b8d074bfe200e75a3dfda491e8d96824
Daniel Alvarez <daniel.yllanes@hotmail.com>
2025-01-28 03:34:46 -0500

    CREATE functionality from react frontend to express api/mongodb atlas working

--- client/src/CreateUser.jsx
+++ client/src/CreateUser.jsx
@@ -1,22 +1,52 @@
-import React from 'react'
+import {React,useState} from 'react'
+import axios from 'axios'
 
 export default function CreateUser(){
+    const [name, setName] = useState()
+    const [email, setEmail] = useState()
+    const [age, setAge] = useState()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        axios.post('http://localhost:2121/createUser', {name,email,age})
+            .then(result => console.log(result))
+            .catch(err=>console.log(err))
+    }
+
     return(
         <div className='d-flex vh-100 bg-primary justify-content-center align-items-center' >
             <div className='w-50 bg-white rounded p-3' >
-                <form>
+                <form onSubmit={handleSubmit}>
                     <h2>Add User</h2>
                     <div className='mb-2'>
-                        <label htmlFor=''>Name:</label>
-                        <input type='text' placeholder='Enter Name' className='form-control' />
+                        <label htmlFor='name'>Name:</label>
+                        <input  type='text' 
+                                id='name'
+                                placeholder='Enter Name' 
+                                name='name'
+                                className='form-control' 
+                                onChange={(e)=>setName(e.target.value)}
+                        />
                     </div>
                     <div className='mb-2'>
-                        <label htmlFor=''>Email:</label>
-                        <input type='email' placeholder='Enter Email' className='form-control' />
+                        <label htmlFor='email'>Email:</label>
+                        <input  type='email' 
+                                id='email'
+                                name='email'
+                                placeholder='Enter Email' 
+                                className='form-control' 
+                                onChange={(e)=>setEmail(e.target.value)}
+                        />
                     </div>
                     <div className='mb-2'>
-                        <label htmlFor=''>Age:</label>
-                        <input type='text' placeholder='Enter Age' className='form-control' />
+                        <label htmlFor='age'>Age:</label>
+                        <input  type='text'
+                                id='age'
+                                name='age' 
+                                placeholder='Enter Age' 
+                                className='form-control'
+                                onChange={(e)=>setAge(e.target.value)} 
+                        />
                     </div>
                     <button className='btn btn-success' >Create User</button>
                 </form>
---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const UserModel = require('./models/Users.js')
+const dotenv = require('dotenv').config({path:'../.env'})
+
+const app = express()
+
+app.use(cors())
+app.use(express.json()) //when sending data from front-end to back-end, will parse data into JSON format
+
+mongoose.connect(process.env.MONGODB_ATLAS_URI)
+
+app.post('/createUser', (req,res)=>{
+    UserModel.create(req.body)
+        .then(users => res.json(users))
+        .catch(err => res.json(err))
+})
+
+app.listen(2121, ()=>{
+    console.log('Express Server is Running on Port 2121!')
+})
+
+
